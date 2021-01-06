c0f598c06cb5a47d16e5b0f67e4e2f757995ed8b
Vinícius Reis <vinicius.moreira2003@gmail.com>
2021-01-06 16:17:38 -0300

    toast message

---
+++ includes/message.php
@@ -0,0 +1,16 @@
+<?php
+// Sessão
+session_start();
+if(isset($_SESSION['mensagem'])): ?>
+    
+<script>
+    // Mensagem em formato toast
+    window.unload = function() {
+        M.toast({html: '<?php echo $_SESSION['mensagem']; ?>'});
+    };
+</script>
+
+<?php
+endif;
+session_unset();
+?>
